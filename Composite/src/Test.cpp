f2c2b6c9eb3f20a8e508557df806c0b6a16fd39f
hscottvo <hscottvo@gmail.com>
2020-12-06 03:11:42 -0800

    Improved layout of test * Quotes are still hard-coded!

--- Composite/src/Test.cpp
+++ Composite/src/Test.cpp
@@ -1,7 +1,7 @@
 #include "../header/Test.hpp"
 
 void Test::run(int max_row, int max_col) {
-    body->set_max_col(2 * max_col / 4);
+    body->set_max_col(3 * max_col / 4);
     body->display(body->get_row(), body->get_col());
 
     for(unsigned i = body->get_input_size(); i < body->get_ref_size(); ++i) {
@@ -11,7 +11,7 @@ void Test::run(int max_row, int max_col) {
 }
 
 void Test::get_text(int max_row, int max_col) {
-    std::vector<std::string> ref = {"The", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog."};
+    std::vector<std::string> ref = {"If", "you", "set", "your", "goals", "ridiculously", "high", "and", "it\'s", "a", "failure,", "you", "will", "fail", "above", "everyone", "else\'s", "success."};
     Passage* a = new Passage(max_row/3, max_col/4, ref);
     body = a;
 }
@@ -21,14 +21,14 @@ std::string Test::get_user_input(int row, int col) {
     std::string ret = "";
     for(;;){
         ch = getch();
-        mvaddstr(row, (col-ret.size())/2, ret.c_str());
+        mvaddstr(5 * row / 6, (col-ret.size())/2, ret.c_str());
         if (ch >= 33 && ch <= 126) {
             ret += static_cast<char>(ch);
         }
         switch(ch) {
             case ' ':
             {
-                move(row, 0);
+                move(5 * row / 6, 0);
                 clrtoeol();
                 return ret;
             }
