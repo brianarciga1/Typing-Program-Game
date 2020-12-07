70fb81f2fd068aa42259912b937083de14a38ea8
hscottvo <hscottvo@gmail.com>
2020-12-06 16:51:54 -0800

    Added timer to test

--- Composite/header/Test.hpp
+++ Composite/header/Test.hpp
@@ -5,17 +5,25 @@
 #include "Passage.hpp"
 #include "Text.hpp"
 #include "Word.hpp"
+#include <ctime>
 #include <string>
 
 class Test {
     private: 
         Passage* body;
+        int timer;
     public: 
         void run(int max_row, int max_col);
 
         void get_text(int max_row, int max_col);
 
-        std::string get_user_input(int row, int col); 
+        std::string get_user_input(int row, int col, time_t start); 
+
+        time_t start_timer();
+
+        void update_timer(time_t start);
+
+        void display_timer(int time, int max_row, int max_col);
 };
 
 
--- Composite/src/Test.cpp
+++ Composite/src/Test.cpp
@@ -4,8 +4,10 @@ void Test::run(int max_row, int max_col) {
     body->set_max_col(3 * max_col / 4);
     body->display(body->get_row(), body->get_col());
 
+    time_t start = start_timer();
+
     for(unsigned i = body->get_input_size(); i < body->get_ref_size(); ++i) {
-        body->add_word(this->get_user_input(max_row - 1, max_col));
+        body->add_word(this->get_user_input(max_row, max_col, start));
         body->display(body->get_row(), body->get_col());
     }
 }
@@ -16,19 +18,21 @@ void Test::get_text(int max_row, int max_col) {
     body = a;
 }
 
-std::string Test::get_user_input(int row, int col) {
+std::string Test::get_user_input(int row, int col, time_t start) {
     int ch;
     std::string ret = "";
     for(;;){
+        update_timer(start);
+        display_timer(timer, row, col);
         ch = getch();
-        mvaddstr(5 * row / 6, (col-ret.size())/2, ret.c_str());
+        mvaddstr(5 * (row-1) / 6, (col-ret.size())/2, ret.c_str());
         if (ch >= 33 && ch <= 126) {
             ret += static_cast<char>(ch);
         }
         switch(ch) {
             case ' ':
             {
-                move(5 * row / 6, 0);
+                move(5 * (row-1) / 6, 0);
                 clrtoeol();
                 return ret;
             }
@@ -39,4 +43,20 @@ std::string Test::get_user_input(int row, int col) {
         }
     
     }
+}
+
+time_t Test::start_timer() { 
+    time_t timer;
+    return time(&timer);
+
+}
+
+void Test::update_timer(time_t start) {
+    time_t curr_time;
+    time(&curr_time);
+    timer = difftime(curr_time, start);
+}
+
+void Test::display_timer(int time, int max_row, int max_col) {
+    mvaddstr(0, max_col/2, std::to_string(time).c_str());
 }
