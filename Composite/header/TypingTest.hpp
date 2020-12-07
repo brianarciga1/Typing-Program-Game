6449f0f347bfe51e123b46758f1efadefd937ec0
hscottvo <hscottvo@gmail.com>
2020-12-07 01:05:52 -0800

    Refactored to TypingTest class, added get_accuracy()

---
+++ Composite/header/TypingTest.hpp
@@ -0,0 +1,34 @@
+#ifndef __TEST_HPP__
+#define __TEST_HPP__
+
+#include "ncurses.h"
+#include "Passage.hpp"
+#include "Text.hpp"
+#include "Word.hpp"
+#include <ctime>
+#include <string>
+
+class TypingTest {
+    private: 
+        Passage* body = nullptr;
+        int timer;
+    public: 
+        void run(int max_row, int max_col);
+
+        void get_text(int max_row, int max_col);
+
+        std::string get_user_input(int row, int col, time_t start); 
+
+        time_t start_timer();
+
+        void update_timer(time_t start);
+
+        void display_timer(int time, int max_row, int max_col);
+
+        std::string get_accuracy(); 
+
+        void set_body(Passage* p) { body = p; }
+};
+
+
+#endif
